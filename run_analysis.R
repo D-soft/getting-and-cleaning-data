1c33d0fac2e6fd11e6fccad174fd4c82aa7d10c8
D-soft <63844408+D-soft@users.noreply.github.com>
2020-06-02 08:25:15 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-# get train data and test data
+# 1.1 get train data and test data
 train.x <- read.table("./train/X_train.txt")
 train.y <- read.table("./train/y_train.txt")
 train.subject <- read.table("./train/subject_train.txt")
@@ -6,29 +6,29 @@ test.x <- read.table("./test/X_test.txt")
 test.y <- read.table("./test/y_test.txt")
 test.subject <- read.table("./test/subject_test.txt")
 
-# merge data set
+# 1.2 merge data set
 trainData <- cbind(train.subject, train.y, train.x)
 testData <- cbind(test.subject, test.y, test.x)
 fullData <- rbind(trainData, testData)
 
-# extract the mean and standard deviation and assign names to data table
+# 2 extract the mean and standard deviation and assign names to data table
 featureName <- read.table("./features.txt", stringsAsFactors = FALSE)[,2]
 featureIndex <- grep(("mean\\(\\)|std\\(\\)"), featureName)
 finalData <- fullData[, c(1, 2, featureIndex + 2)]
 colnames(finalData) <- c("subject", "activity", featureName[featureIndex])
 
-# use the activity names to name the activity in the data set
+# 3 uses descriptive activity names to name the activities in the data set
 activityName <- read.table("./activity_labels.txt")
 finalData$activity <- factor(finalData$activity, levels = activityName[,1], labels = activityName[,2])
 
-# make the names readable
+# 4 make the variable names readable
 names(finalData) <- gsub("\\()", "", names(finalData))
 names(finalData) <- gsub("^t", "time", names(finalData))
 names(finalData) <- gsub("^f", "frequence", names(finalData))
 names(finalData) <- gsub("-mean", "Mean", names(finalData))
 names(finalData) <- gsub("-std", "Std", names(finalData))
 
-# calculate average of each variable and order the data by subject and by activity
+# 5 calculate average of each variable and order the data by subject and by activity
 # Finally, produce the tidy data
 library(dplyr)
 groupData <- finalData %>%
